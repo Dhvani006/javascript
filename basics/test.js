5c40eb962db2aaa5d608fe26411f2467a3c38b18
Dhvani006 <dhvanikotak006@gmail.com>
2024-12-18 12:52:08 +0000

    container config done

--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,7 @@
 console.log("dk")
+
+//console.table([var name..])
+//let use for scop
+//var use for globle scop
+//without let and var variable_name=value possible
+//let variablename; console.log(variablename) output come undefined
